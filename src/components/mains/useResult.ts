272d286aed04cd8968ddbf82f6243abd3bc8ca0e
ella hain <ehain@bu.edu>
2025-10-21 21:00:55 -0400

    updated type in custom hook

--- src/components/mains/useResult.ts
+++ src/components/mains/useResult.ts
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react';
 
 /*this Custom Hook is for testing whether the answers are positive or negative*/
 function useResult(result: string): boolean {
-    const [positive, setPositive] = useState(true);
+    const [positive, setPositive] = useState<boolean>(true);
     useEffect(() => {
         if (Number(result) < 0){
             setPositive(false);
